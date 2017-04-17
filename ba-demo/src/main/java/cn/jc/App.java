fa8a672c22c69e8a8567e1a657841bf01ae8d2cb
ljianchao <ljianchao@163.com>
2017-04-17 17:56:58 +0800

    完善ArrayList的增加和删除功能

--- ba-demo/src/main/java/cn/jc/App.java
+++ ba-demo/src/main/java/cn/jc/App.java
@@ -3,9 +3,11 @@ package cn.jc;
 import cn.jc.design.proxy.GamePlayIH;
 import cn.jc.design.proxy.GamePlayer;
 import cn.jc.design.proxy.GamePlayerImpl;
+import cn.jc.util.ArrayList;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+import java.util.List;
 
 /**
  * Hello world!
@@ -26,13 +28,49 @@ public class App
 //        proxy.upgrade();
 
         // 测试ArrayList
-        cn.jc.util.List<String> strList = new cn.jc.util.ArrayList<>();
-        strList.add("Jack");
-        strList.add("Liu");
-        cn.jc.util.List<String> strListClone = new cn.jc.util.ArrayList<>(strList);
-        for (String str :
-                strListClone) {
-            System.out.println(str);
+//        cn.jc.util.List<String> strList = new cn.jc.util.ArrayList<>();
+//        strList.add("Jack");
+//        strList.add("Liu");
+//        cn.jc.util.List<String> strListClone = new cn.jc.util.ArrayList<>(strList);
+//        for (String str :
+//                strListClone) {
+//            System.out.println(str);
+//        }
+//
+//        cn.jc.util.ArrayList<String> defaultList = new cn.jc.util.ArrayList<>();
+//        defaultList.add("1");
+//        defaultList.ensureCapacity(11);
+
+//        String[] as = {"1","2", "3"};
+//        int index = 0;
+//        System.out.println(as[index++]);
+//        System.out.println(as[index++]);
+//        System.out.println(as[index++]);
+
+        // 测试ArrayList的浅表副本clone()
+        ArrayList<User> oriList = new ArrayList<>();
+        oriList.add(new User("L", 1));
+        oriList.add(new User("J", 2));
+        ArrayList<User> cloneList = (ArrayList)oriList.clone();
+
+        System.out.println("original list:");
+        for (User user:
+             oriList) {
+            System.out.println("name=" + user.getName() + "; age=" + user.getAge());
         }
+        cloneList.get(1).setName("X");
+
+        System.out.println("original list:");
+        for (User user:
+                oriList) {
+            System.out.println("name=" + user.getName() + "; age=" + user.getAge());
+        }
+
+        System.out.println("destination list:");
+        for (User user:
+                oriList) {
+            System.out.println("name=" + user.getName() + "; age=" + user.getAge());
+        }
+
     }
 }
--- ba-demo/src/main/java/cn/jc/util/ArrayList.java
+++ ba-demo/src/main/java/cn/jc/util/ArrayList.java
@@ -45,6 +45,25 @@ public class ArrayList<E> extends AbstractList<E>
         }
     }
 
+    public void trimToSize(){
+        // TODO:为什么modCount要更新？
+        modCount++;
+        if (size < elementData.length){
+            elementData = (size == 0)
+                    ? EMPTY_ELEMENTDATA
+                    : Arrays.copyOf(elementData, size);
+        }
+    }
+
+    public void ensureCapacity(int minCapacity){
+        int minExpand = (elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA)
+                ? 0
+                : DEFAULT_CAPACITY;
+        if (minCapacity > minExpand){
+            ensureExplicitCapacity(minCapacity);
+        }
+    }
+
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
 
     private void ensureCapacityInternal(int minCapacity){
@@ -85,6 +104,60 @@ public class ArrayList<E> extends AbstractList<E>
                 MAX_ARRAY_SIZE;
     }
 
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean contains(Object o){
+        return indexOf(o) > 0;
+    }
+
+    public int indexOf(Object o){
+        if (o == null){
+            for (int i = 0; i < size; i++)
+                if (elementData[i] == null)
+                    return i;
+        } else {
+            for (int i = 0; i < size; i++)
+                if (o.equals(elementData[i]))
+                    return i;
+        }
+
+        return -1;
+    }
+
+    public int lastIndexOf(Object o){
+        if (o == null){
+            for (int i = size - 1; i >= 0; i--)
+                if (elementData[i] == null)
+                    return i;
+        } else {
+            for (int i = size - 1; i >= 0; i--)
+                if (o.equals(elementData[i]))
+                    return i;
+        }
+        return -1;
+    }
+
+    // 浅表副本
+    // 元素是引用类型，复制的只是对象的引用
+    public Object clone(){
+        try {
+            ArrayList<?> v = (ArrayList<?>) super.clone();
+            // elementData是protect访问权限
+            v.elementData = Arrays.copyOf(elementData, size);
+            v.modCount = 0;
+            return v;
+        } catch (CloneNotSupportedException e) {
+            throw new InternalError(e);
+        }
+    }
+
     E elementData(int index){
         return (E) elementData[index];
     }
@@ -111,15 +184,103 @@ public class ArrayList<E> extends AbstractList<E>
         return elementData(index);
     }
 
-    @Override
-    public int size() {
-        return size;
+    public E set(int index, E element){
+        rangeCheck(index);
+
+        E oldValue = elementData(index);
+        elementData[index] = element;
+        return oldValue;
     }
 
     public boolean add(E e){
         ensureCapacityInternal(size + 1);   // increments modCount!!
         elementData[size++] = e;
         return true;
+    }
+
+    public void add(int index, E element){
+        rangeCheckForAdd(index);
+
+        ensureCapacityInternal(size + 1);
+        System.arraycopy(elementData, index, elementData, index + 1,
+                size - index);  // elemantData数组进行数组移动
+        elementData[index] = element;
+        size++;
+    }
+
+    public E remove(int index){
+        rangeCheck(index);
+
+        modCount++;
+        E oldValue = elementData(index);
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index + 1, elementData, index,
+                    numMoved);
+        elementData[--size] = null; // clear to let GC do its work
+
+        return oldValue;
+    }
+
+    public boolean remove(Object o){
+        if (o == null){
+            for (int index = 0; index < size; index++)
+                if (elementData[index] == null){
+                    fastRemove(index);
+                    return true;
+                }
+        } else {
+            for (int index = 0; index < size; index++)
+                if (o.equals(elementData[index])){
+                    fastRemove(index);
+                    return true;
+                }
+        }
+
+        return false;
+    }
+
+    private void fastRemove(int index){
+        modCount++;
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index + 1, elementData, index,
+                        numMoved);
+        elementData[--size] = null;
+    }
+
+    public void clear(){
+        modCount++;
+
+        for (int i = 0; i < size; i++)
+            elementData[i] = null;
+
+        size = 0;
+    }
+
+    public boolean addAll(Collection<? extends E> c){
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacityInternal(size + numNew);
+        System.arraycopy(a, 0, elementData, size, numNew);
+        size += numNew;
+        return numNew != 0;
+    }
+
+    public boolean addAll(int index, Collection<? extends E> c){
+        rangeCheckForAdd(index);
+
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacityInternal(size + numNew);
+
+        int numMoved = size - index;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index, elementData, index + numNew,
+                    numMoved);  //先移动原数组
+        System.arraycopy(a, 0, elementData, index, numNew);   // 添加新数组元素
+        size += numNew;
+        return numNew != 0;
     }
 
     private void rangeCheck(int index){
